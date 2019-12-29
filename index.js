a1ff4897ba81e2fa38d18f982416eedd851ff720
Mark Artishuk <markyshuk@gmail.com>
2019-12-28 21:23:13 -0800

    Finished setting up db tables and now getting ready for auth router

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+if (!process.env.NODE_ENV || process.env.NODE_ENV != "production")
+  require("dotenv").config();
+
 const server = require("./server");
 const port = process.env.PORT || 4000;
 
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "5h"
+  };
+  return jwt.sign(payload, process.env.JWT_SECRET_KEY, options);
+}
 
 module.exports = router;
