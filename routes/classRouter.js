3d98b1fabf7ee46b7bf5004c67210e16d0624bcd
Mark Artishuk <markyshuk@gmail.com>
2020-01-03 02:00:23 -0800

    Added many class model functions

--- routes/classRouter.js
+++ routes/classRouter.js
@@ -1,5 +1,34 @@
 const express = require("express");
-
+const Classes = require("../models/classModel");
 const router = express.Router();
+
+// Need instructor id, name, type, startTime, duration, intensity, location, and maxSize
+router.post("/", (req, res) => {
+  const body = req.body;
+  if (
+    !body.instructor_id ||
+    !body.name ||
+    !body.type ||
+    !body.startTime ||
+    !body.duration ||
+    !body.intensity ||
+    !body.location ||
+    !body.maxSize
+  ) {
+    res.status(400).json({
+      message:
+        "instructor_id, name, type, startTime, duration, intensity, location, and maxSize fields are required"
+    });
+  } else {
+    Classes.add(body)
+      .then(obj => {})
+      .catch(err => {
+        console.log("Error making class", err);
+        res.status(500).json({
+          message: "There was an error in the server trying to add the class"
+        });
+      });
+  }
+});
 
 module.exports = router;
