745e70e0f12ea30cd10661fac9d46d038ef7a598
Mark Artishuk <markyshuk@gmail.com>
2020-01-06 12:22:57 -0800

    getting class by id now also returns the instructors username

--- models/classModel.js
+++ models/classModel.js
@@ -47,9 +47,21 @@ function removeById(id) {
 }
 
 function findById(id) {
-  return db("classes")
-    .select("*")
-    .where({ id })
+  return db("classes as cl")
+    .select(
+      "cl.id",
+      "cl.instructor_id",
+      "u.username as instructor_name",
+      "cl.name",
+      "cl.type",
+      "cl.startTime",
+      "cl.duration",
+      "cl.intensity",
+      "cl.location",
+      "cl.maxSize"
+    )
+    .join({ u: "users" }, "cl.instructor_id", "=", "u.id")
+    .where({ "cl.id": id })
     .first();
 }
 
