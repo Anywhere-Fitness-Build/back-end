494e1363444eb3cd0b3fb4a5e18ba181d5b7b775
Mark Artishuk <markyshuk@gmail.com>
2020-01-06 11:19:23 -0800

    Changing seeds to support more postgres

--- database/seeds/01_roles.js
+++ database/seeds/01_roles.js
@@ -1,12 +1,7 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("roles")
-    .truncate()
-    .then(function() {
-      // Inserts seed entries
-      return knex("roles").insert([
-        { id: 1, roleName: "client" },
-        { id: 2, roleName: "instructor" }
-      ]);
-    });
+  return knex("roles").insert([
+    { id: 1, roleName: "client" },
+    { id: 2, roleName: "instructor" }
+  ]);
 };
